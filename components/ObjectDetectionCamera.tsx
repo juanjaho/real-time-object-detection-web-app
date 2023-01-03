5d7560bbc1be9e1e52786ed9d9d7499b2f8ea33e
juanjaho <juansebastianjahowidjaja@gmail.com>
2023-01-04 00:35:19 +0800

    first commit

---
+++ components/ObjectDetectionCamera.tsx
@@ -0,0 +1,28 @@
+// import React, { useRef, useEffect } from "react";
+
+// const VideoCamera = (props: any) => {
+//   const videoRef = useRef<HTMLVideoElement>(null);
+
+//   useEffect(() => {
+//     navigator.mediaDevices.getUserMedia({ video: true }).then((stream) => {
+//       if (videoRef.current) {
+//         videoRef.current.srcObject = stream;
+//       } else {
+//         console.log("videoRef.current is null");
+//       }
+//     });
+//   }, []);
+
+//   return (
+//     <video ref={videoRef} width={props.width} height={props.height} autoPlay />
+//   );
+// };
+
+// export default VideoCamera;
+
+import React from "react";
+import Webcam from "react-webcam";
+
+const WebcamComponent = (props: any) => <Webcam />;
+
+export default WebcamComponent;
